f81a6404f75c765f622f96ee296b34be0b36a8a8
sarada12ru <52966865+sarada12ru@users.noreply.github.com>
2019-07-19 10:07:58 +0530

    Update MainActivity.java

--- app/src/main/java/com/extractnew/App1/MainActivity.java
+++ app/src/main/java/com/extractnew/App1/MainActivity.java
@@ -2,11 +2,8 @@ package com.extractnew.App1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -36,26 +33,12 @@ public class MainActivity extends AppCompatActivity
                 {
                     img.setLeft(612);
                     img.setRight(1038);
-                    ObjectAnimator animation = ObjectAnimator.ofFloat(view, "translationX", 100f);
-                    animation.setDuration(2000);
-                    animation.start();
                 }
                 else if(left == 612 && right == 1038)
                 {
                     img.setLeft(20);
                     img.setRight(446);
-                    ObjectAnimator animation = ObjectAnimator.ofFloat(view, "translationX", 100f);
-                    animation.setDuration(2000);
-                    animation.start();
                 }
-
-//                Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.left_to_right);
-//                img.startAnimation(animation);
-//                img.setVisibility(View.VISIBLE);
-                /*System.out.println("Left :-"+img.getLeft());
-                System.out.println("Right :-"+img.getRight());
-                System.out.println("Top :-"+img.getTop());
-                System.out.println("Bottom :-"+img.getBottom());*/
 
             }
         });
